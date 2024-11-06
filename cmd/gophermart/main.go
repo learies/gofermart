26a25bac39a68e136cee64fa52b574bbea3be3f3
LeAries <learies@ya.ru>
2024-11-06 13:36:10 +0300

    Add config load flags

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/learies/gofermart/internal/app"
+	"github.com/learies/gofermart/pkg/config"
 )
 
 func main() {
+	// Load configuration
+	cfg := config.NewConfig()
+
+	// Initialize the application
 	application := app.NewApp()
 	defer application.Close()
 
-	log.Println("Starting server on :8080")
-	if err := http.ListenAndServe(":8080", application.Routes); err != nil {
-		log.Fatal(err)
+	fmt.Println(cfg.DatabaseURI)
+
+	// Start the HTTP server
+	log.Printf("Starting server on %s\n", cfg.RunAddress)
+	if err := http.ListenAndServe(cfg.RunAddress, application.Routes); err != nil {
+		log.Fatalf("Could not start server: %v", err)
 	}
 }
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,13 +1,45 @@
 package config
 
+import (
+	"flag"
+	"os"
+)
+
 // Config struct holds the configuration values for the application
 type Config struct {
-	ServerPort string
+	ServerHost  string
+	ServerPort  string
+	RunAddress  string
+	DatabaseURI string
 }
 
-// LoadConfig loads the application configuration
-func LoadConfig() *Config {
-	return &Config{
-		ServerPort: ":8080",
+// NewConfig creates a new Config instance and loads the configuration
+func NewConfig() *Config {
+	cfg := &Config{}
+	cfg.loadFlags()
+	return cfg
+}
+
+func getEnv(key string, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
 	}
+	return fallback
+}
+
+// loadFlags parses command-line flags and populates the Config struct
+func (cfg *Config) loadFlags() {
+	const (
+		defaultAddress     = "localhost:8080"
+		defaultPostgresURI = "postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable"
+	)
+
+	// Load environment variables
+	cfg.RunAddress = getEnv("RUN_ADDRESS", defaultAddress)
+	cfg.DatabaseURI = getEnv("DATABASE_URI", defaultPostgresURI)
+
+	// Define command-line flags
+	flag.StringVar(&cfg.RunAddress, "a", cfg.RunAddress, "run address (default: localhost:8080)")
+	flag.StringVar(&cfg.DatabaseURI, "d", cfg.DatabaseURI, "database URI (default: postgres://postgres:postgres@localhost:5432/postgres?sslmode=disable)")
+	flag.Parse()
 }
